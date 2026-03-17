01bf8f3975e9857f755403ccfe5e8a9eb73f3b55
Garth Trickett <garthtrickett@gmail.com>
2026-03-17 17:21:05 +1100

    fix: sitemap linting errors

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,4 +1,5 @@
 import { MetadataRoute } from 'next';
+import { PageDocument } from '#prismicio-types.js'; 
 import { createClient } from '@/prismicio';
 import ghost from '@/lib/ghost';
 import { getIndustries, getComparisons } from '@/lib/outrank';
